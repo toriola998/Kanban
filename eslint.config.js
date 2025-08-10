bc4c0a3b15cffa0b575d7deb5b50b153c24cec9f
Toriola <faaedah@gmail.com>
2025-08-10 21:12:43 +0100

    SETUP: scafolded project

---
+++ eslint.config.js
@@ -0,0 +1,42 @@
+import js from "@eslint/js";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import { defineConfig, globalIgnores } from "eslint/config";
+
+export default defineConfig([
+   globalIgnores(["dist"]),
+   {
+      files: ["**/*.{js,jsx}"],
+      extends: [
+         js.configs.recommended,
+         reactHooks.configs["recommended-latest"],
+         reactRefresh.configs.vite,
+         prettier,
+      ],
+      languageOptions: {
+         ecmaVersion: 2020,
+         globals: globals.browser,
+         parserOptions: {
+            ecmaVersion: "latest",
+            ecmaFeatures: { jsx: true },
+            sourceType: "module",
+         },
+      },
+      plugins: ["prettier"],
+      rules: {
+         "no-unused-vars": ["error", { varsIgnorePattern: "^[A-Z_]" }],
+         "prettier/prettier": [
+            "error",
+            {
+               singleQuote: true,
+               semi: false,
+               trailingComma: "es5",
+               endOfLine: "auto",
+               printWidth: 80,
+               tabWidth: 3,
+            },
+         ],
+      },
+   },
+]);
---
+++ src/main.jsx
@@ -0,0 +1,34 @@
+import { StrictMode } from "react";
+import App from "./routes/App.jsx";
+
+//import { createRoot } from 'react-dom/client'
+import "./styles/index.css";
+
+// createRoot(document.getElementById('root')).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>,
+// )
+import { createBrowserRouter, RouterProvider } from "react-router";
+import ReactDOM from "react-dom/client";
+
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+const router = createBrowserRouter([
+   {
+      path: "/",
+      element: <App />,
+   },
+]);
+
+const root = document.getElementById("root");
+
+ReactDOM.createRoot(root).render(
+   <>
+      <ToastContainer theme="colored" />
+      <StrictMode>
+         <RouterProvider router={router} />
+      </StrictMode>
+   </>,
+);
